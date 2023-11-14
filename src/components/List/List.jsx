c70e67cc7e5a61f0a68b7542a05adb17fc5eabb7
Melina Schild <melina.schild@icloud.com>
2023-11-14 11:02:11 +0100

    card erstes styling

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,17 @@
+import SearchResultCards from "../SearchResultCards/SearchResultCards";
 import styles from "./List.module.css";
 
 const List = () => {
-  return <section className={styles.search_result_list}></section>;
+  return (
+    <section className={styles.search_result_list}>
+      <SearchResultCards
+        strMealThumb="https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg"
+        strMeal="Irgendeine Pasta"
+        strCategory="Random Category"
+        idMeal="52771"
+      />
+    </section>
+  );
 };
 
 export default List;
--- src/components/SearchResultCards/SearchResultCards.jsx
+++ src/components/SearchResultCards/SearchResultCards.jsx
@@ -1,23 +1,39 @@
+import { Link } from "react-router-dom";
 import styles from "./SearchResultCards.module.css";
 
 const SearchResultCards = (props) => {
   return (
     <article className={styles.card}>
       <div className={styles.container}>
-        <div className={styles.imgContainer}>
+        {/*  */}
+        <div className={styles.img_container}>
           <img
             className={styles.image}
             src={props.strMealThumb}
             alt="meal image"
           />
         </div>
-        <div className={styles.textContainer}>
-          <p className={styles.name}>{props.strMeal}</p>
+        {/*  */}
+        {/*  */}
+        <div className={styles.text_container}>
+          <div className={styles.meal_container}>
+            <p className={styles.name}>{props.strMeal}</p>
+            <div className={styles.category}>
+              <img src="/public/circle.svg" alt="icon" />
+              <p>{props.strCategory}</p>
+            </div>
+          </div>
+
+          {/*  */}
           <div>
-            <img src="#" alt="icon" />
-            <p>{props.strCategory}</p>
+            <Link to={`/details/${props.idMeal}`}>
+              <button className={styles.button}>
+                <img src="/public/arrow-white.svg" alt="link to meal" />
+              </button>
+            </Link>
           </div>
         </div>
+        {/*  */}
       </div>
     </article>
   );
