d7b5e77bde4974a11a5d110ff204195117893863
Motahare <ghanbari_ustmb@yahoo.com>
2023-11-15 13:06:11 +0100

    Searchbox in homepage und home styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,107 @@
-
 import { useEffect, useState } from "react";
 import FetchAPI from "../functions/FetchAPI";
 import AreasSlider from "../components/sliders/AreasSlider";
 import CategoriesSlider from "../components/sliders/CategoriesSlider";
 import MealOfTheDay from "../components/MealOfTheDay";
 import SearchBox from "../components/SearchBox";
-import styles from "./Home.module.css";
+import List from "../components/List/List";
+import DataLoader from "../components/searchpage/DataLoader";
 
+import styles from "./Home.module.css";
+import loadingStyles from "./SearchPage.module.css";
 const Home = ({ children }) => {
+  const [searchInput, setSearchInput] = useState("");
+  const [currentData, setCurrentData] = useState([]);
+  const [randomMeal, setRandomMeal] = useState();
+  const [areas, setAreas] = useState([]);
+  const [categorys, setCategorys] = useState([]);
+
+  const fetchDataforName = async (_val) => {
+    setSearchInput(_val);
+    const response = await FetchAPI(`search.php?s=${_val}`);
+    setCurrentData(response);
+    console.log("Von Home ==>", response);
+  };
+
+  useEffect(() => {
+    async function fetchForRandomMeal() {
+      try {
+        const response = await FetchAPI("random.php");
+        if (response && response.meals) {
+          console.log("response.meals", response.meals);
+          const mealItem = {
+            mealId: response.meals[0].idMeal,
+            mealName: response.meals[0].strMeal,
+            mealImg: response.meals[0].strMealThumb,
+            mealArea: response.meals[0].strArea,
+            mealCategorie: response.meals[0].strCategory,
+          };
+          setRandomMeal(mealItem);
+        }
+      } catch (err) {
+        console.error("Fehler beim Laden der Daten:", err);
+      }
+    }
+
+    async function fetchDataForArea() {
+      try {
+        const response = await FetchAPI("list.php?a=list");
+        if (response && response.meals) {
+          const areasItem = response.meals.map((area) => ({
+            description: area.strArea,
+          }));
+          setAreas(areasItem);
+        }
+      } catch (err) {
+        console.error("Fehler beim Laden der Daten:", err);
+      }
+    }
+
+    async function fetchForCategory() {
+      try {
+        const response = await FetchAPI("categories.php");
+        if (response && response.categories) {
+          const categorysItem = response.categories.map((category) => ({
+            categoryId: category.idCategory,
+            categoryName: category.strCategory,
+            categoryImg: category.strCategoryThumb,
+            categoryDescription: category.strCategoryDescription,
+          }));
+          setCategorys(categorysItem);
+        }
+      } catch (err) {
+        console.error("Fehler beim Laden der Daten:", err);
+      }
+    }
+    fetchForRandomMeal();
+    fetchDataForArea();
+    fetchForCategory();
+  }, []);
+
   return (
     <>
       <section className={styles.container}>
         <header className={styles.header}>
-          <SearchBox />
+          <SearchBox
+            searchInput={searchInput}
+            onHandleSearchByType={fetchDataforName}
+          />
         </header>
-        <main className={styles.main}>
-          <MealOfTheDay />
-          <AreasSlider />
-          <CategoriesSlider />
-        </main>
+        {searchInput && currentData ? (
+          <List currentData={currentData} categories="home" />
+        ) : randomMeal && areas && categorys ? (
+          <main className={styles.main}>
+            <MealOfTheDay randomMeal={randomMeal} />
+            <AreasSlider areas={areas} />
+            <CategoriesSlider categorys={categorys} />
+          </main>
+        ) : (
+          <DataLoader
+            styles={loadingStyles.loading}
+            loadingText="LOADING...."
+          />
+        )}
+
         {children}
       </section>
     </>
