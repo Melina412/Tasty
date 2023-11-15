8f6b3649755ecb8ffa12db24840196c5a0b3d528
oschwill <o.schwill@web.de>
2023-11-15 09:06:38 +0100

    Navbar as child / navbar highlighting

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,75 @@
-import "./App.css";
+import './App.css';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
-import Onboarding from "./pages/Onboarding";
-import Home from "./pages/Home";
-import SearchPage from "./pages/SearchPage";
-import DetailPage from "./pages/DetailPage";
+import Onboarding from './pages/Onboarding';
+import Home from './pages/Home';
+import SearchPage from './pages/SearchPage';
+import DetailPage from './pages/DetailPage';
 
-import Loadingscreen from "./pages/Loadingscreen";
-import List from "./components/List/List";
+import Loadingscreen from './pages/Loadingscreen';
+import List from './components/List/List';
 
 //* UseContext für Darkmode und Loadingscreen
-import { LoadingContext } from "./context/Context";
-import { ThemeContext } from "./context/Context";
+import { LoadingContext } from './context/Context';
+import { ThemeContext } from './context/Context';
+import NavBar from './components/NavBar';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
 
 function App() {
   const [theme, setTheme] = useState(false);
   const [loading, setLoading] = useState(false);
 
   return (
-    <section className={theme ? "dark" : null}>
+    <section className={theme ? 'dark' : null}>
       <ThemeContext.Provider value={{ theme, setTheme }}>
         <LoadingContext.Provider value={{ loading, setLoading }}>
           {loading ? (
             <BrowserRouter>
               <Routes>
                 <Route path="/" element={<Onboarding />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/search/:type/:name" element={<SearchPage />} />
-                <Route path="/detail/:id" element={<DetailPage />} />
-                <Route path="/list" element={<List />} />
+                <Route
+                  path="/home"
+                  element={
+                    <Home>
+                      <NavBar activeName="home" />
+                    </Home>
+                  }
+                />
+                <Route
+                  path="/search/:type/:name"
+                  element={
+                    <SearchPage>
+                      <NavBar activeName="search" />
+                    </SearchPage>
+                  }
+                />
+                <Route
+                  path="/detail/:id"
+                  element={
+                    <DetailPage>
+                      <NavBar />
+                    </DetailPage>
+                  }
+                />
+                <Route
+                  path="/favorites"
+                  element={
+                    <Favorites>
+                      <NavBar activeName="favorites" />
+                    </Favorites>
+                  }
+                />
+                <Route
+                  path="/profile"
+                  element={
+                    <Profile>
+                      <NavBar activeName="profile" />
+                    </Profile>
+                  }
+                />
               </Routes>
             </BrowserRouter>
           ) : (
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+const Favorites = ({ children }) => {
+  return (
+    <main>
+      <h1 style={{ fontSize: '3rem' }}>FAVORITES!!</h1>
+      {children}
+    </main>
+  );
+};
+
+export default Favorites;
