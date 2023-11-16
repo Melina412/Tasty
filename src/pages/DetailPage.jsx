3e8cc5c0dd2729ea07cdb810dcc25291d3b2660b
oschwill <o.schwill@web.de>
2023-11-16 14:00:48 +0100

    localstorage eingebunden / profilseite angepasst

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,11 +1,12 @@
-import { useState, useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
-import FetchAPI from "../functions/FetchAPI";
-import { FavoriteContext } from "../context/Context";
-import { ThemeContext } from "../context/Context";
+import { useState, useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import FetchAPI from '../functions/FetchAPI';
+import { FavoriteContext } from '../context/Context';
+import { ThemeContext } from '../context/Context';
 
 import styles from '../pages/Detailpage.module.css';
 import VideoPlayer from '../components/player/VideoPlayer';
+import useLocalStorage from '../functions/useLocalStorage';
 
 const DetailPage = ({ children }) => {
   const { theme } = useContext(ThemeContext);
@@ -16,6 +17,8 @@ const DetailPage = ({ children }) => {
   // - für die id vom Meal of the Day
   const idParams = useParams();
   const id = idParams.id;
+
+  const [localFavorite, setLocalFavorite] = useLocalStorage('favorites', favorite);
 
   useEffect(() => {
     async function fetchData() {
@@ -34,8 +37,6 @@ const DetailPage = ({ children }) => {
 
   const ifchecked = favorite.some((item) => item.idMeal === id);
 
-  console.log(singleMeal);
-
   const [toggle, setToggle] = useState(true);
 
   const toggleFunction = () => {
@@ -43,11 +44,17 @@ const DetailPage = ({ children }) => {
   };
 
   const handleSetFavorites = () => {
-    favorite.some((item) => item.idMeal === id)
-      ? setFavorite((currentFavorites) =>
-          currentFavorites.filter((cur) => cur.idMeal !== singleMeal[0].idMeal)
-        )
-      : setFavorite((currentFavorites) => [...currentFavorites, singleMeal[0]]);
+    if (favorite.some((item) => item.idMeal === id)) {
+      setFavorite((currentFavorites) =>
+        currentFavorites.filter((cur) => cur.idMeal !== singleMeal[0].idMeal)
+      );
+      setLocalFavorite((currLocal) =>
+        currLocal.filter((cur) => cur.idMeal !== singleMeal[0].idMeal)
+      );
+    } else {
+      setFavorite((currentFavorites) => [...currentFavorites, singleMeal[0]]);
+      setLocalFavorite([...localFavorite, singleMeal[0]]);
+    }
   };
 
   const handleSetShownVideo = () => {
@@ -58,7 +65,7 @@ const DetailPage = ({ children }) => {
   return (
     <>
       {singleMeal ? (
-        <section className={`${styles.mediaflex} ${theme ? styles.dark : ""}`}>
+        <section className={`${styles.mediaflex} ${theme ? styles.dark : ''}`}>
           <img
             className={`${styles.img}`}
             src={singleMeal[0].strMealThumb}
@@ -150,15 +157,15 @@ const DetailPage = ({ children }) => {
               )}
             </article>
           )}
-            <div className={`${styles.icon}`}>
-              <a target="_blank" href="https://icons8.com/icon/581/herzen">
-                Herz
-              </a>{" "}
-              <p> Icon von </p>
-              <a target="_blank" href="https://icons8.com">
-                Icons8
-              </a>
-            </div>
+          <div className={`${styles.icon}`}>
+            <a target="_blank" href="https://icons8.com/icon/581/herzen">
+              Herz
+            </a>{' '}
+            <p> Icon von </p>
+            <a target="_blank" href="https://icons8.com">
+              Icons8
+            </a>
+          </div>
           {children}
         </section>
       ) : (
